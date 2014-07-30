445b41b9b9491f48996ca82a453cad07450fd699
peteruithoven <peter@peteruithoven.nl>
2014-07-31 00:34:03 +0200

    Separated style files and using gulp to concat

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,28 @@ var concat = require('gulp-concat');
 
 var paths = {
   tests: 'tests/',
-	scripts: ['js/libs/svg.js',
-						'js/libs/*.js',
-						'js/*/*.js',
-						'js/*.js'],
-	testScripts: ['!js/main.js',
-								'js/libs/svg.js',
+  styles: ['css/libs/*.css',
+           'css/*.css'],
+  testScripts: ['js/libs/svg.js',
 								'js/libs/*.js',
 								'js/*/*.js',
-								'js/*.js']
+								'js/*.js']	
 };
+paths.scripts = paths.testScripts.concat()
+paths.scripts.push('js/main.js');
+
+console.log("paths.testScripts: ",paths.testScripts);
+console.log("paths.scripts: ",paths.scripts);
+
+gulp.task('styles', function() {
+	return gulp.src(paths.styles)
+    .pipe(concat('plotter.css'))
+    .pipe(gulp.dest('www/css'));
+});
 
 gulp.task('scripts', function() {
 	return gulp.src(paths.scripts)
-    .pipe(concat('plotter.min.js'))
+    .pipe(concat('plotter.js'))
     .pipe(gulp.dest('www/js'));
 });
 
@@ -34,9 +42,9 @@ gulp.task('test', ['testScripts'], function() {
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-	gulp.watch(paths.scripts, ['scripts']);
+	gulp.watch([paths.scripts,paths.styles], ['styles','scripts']);
 });
 
 
-gulp.task('default', ['scripts','watch']);
+gulp.task('default', ['styles','scripts','watch']);
 
